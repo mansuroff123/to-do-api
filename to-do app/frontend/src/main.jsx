a12f1774ea6fdd4c0ac5cb1d3463dcf7ebe884a9
mansuroff123 <mansuroff145@gmail.com>
2025-08-15 22:26:22 +0500

    app.jsx error fixed

--- to-do app/frontend/src/main.jsx
+++ to-do app/frontend/src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import App from './App'
 
 const qc = new QueryClient()
 createRoot(document.getElementById('root')).render(
